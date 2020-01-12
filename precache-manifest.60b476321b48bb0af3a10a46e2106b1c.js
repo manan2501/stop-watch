b345eac524060f8e36e1fc49c4bfad610626ed24
manan2501 <kevadiyamanan@gmail.com>
2020-01-12 15:19:20 +0530

    Updates

---
+++ precache-manifest.60b476321b48bb0af3a10a46e2106b1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3724b96fabf44dc3dccdd99868e3a92d",
+    "url": "/stop-watch/index.html"
+  },
+  {
+    "revision": "91ebb2ea362896ba994a",
+    "url": "/stop-watch/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "91ebb2ea362896ba994a",
+    "url": "/stop-watch/static/js/2.defdf58d.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/stop-watch/static/js/2.defdf58d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "074f4a800ea9d9be546f",
+    "url": "/stop-watch/static/js/main.ff6b071b.chunk.js"
+  },
+  {
+    "revision": "1232c791206cd27a3892",
+    "url": "/stop-watch/static/js/runtime-main.37348f9c.js"
+  }
+]);
